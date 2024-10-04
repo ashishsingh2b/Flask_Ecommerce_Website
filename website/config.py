2d5e04e3bd40234ee4ab5425047d84855336f9bb
Ashish Singh <ashishsingh2b@gmail.com>
2024-10-04 17:50:35 +0530

    flask ecommerceproject commit

---
+++ website/config.py
@@ -0,0 +1,6 @@
+# config.py
+
+class Config:
+    PAYPAL_CLIENT_ID = 'Ac-6BenOMQUWhSKfhyApy0Hpgmr_ZgM2dK_IHPF62ihajDu-Jxj6BKUyErxeh2n_1tSvZ-JMaWpYbPBH'
+    PAYPAL_CLIENT_SECRET = 'EGDEo0-Zf5zW8HZLGB0fG8_PLr1RqclDrA_bZHcUbU3c78TqnoIKFaZD_cnF3ZS2DRwB6tqa7I7HNZNC'
+    PAYPAL_MODE = 'sandbox'  # Change to 'live' for production
